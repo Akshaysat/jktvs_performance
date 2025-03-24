d4c153422b285c9941d2e4550b87b9fd7c9a1827
Akshay Satpaise <akshay.satpaise99@gmail.com>
2025-03-24 17:13:08 +0530

    updated port for production

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def get_client_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=5002) # for local
+    #app.run(debug=False, host='0.0.0.0', port=8053) # for production
